56f3a4de1a459a4ab0c032de8c9231575e2edda4
leul <leulzewduchanie@gmail.com>
2023-12-12 10:52:45 +0300

    make the alphabet to appear one by one

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import { RiArrowRightLine } from 'react-icons/ri';
 import Hero from "./assets/hero.jpg";
 import {Link} from 'react-scroll'
+import { useState, useEffect } from 'react';
 import Portfolio from './components/Portfolio';
 
 function Home() {
-  
+
+  const [text,setText]=useState('')
+  const [isvisible,setVisible]=useState(false)
+  useEffect(()=>{
+
+    const original = "Im a React , Tailwindcss  and C++ developer"
+    let currentIndex=0;
+    const interval = setInterval(()=>{
+      setText(original.substring(0,currentIndex+1))
+      currentIndex++;
+      if(original.length==currentIndex){
+         setVisible(true);
+         clearInterval(interval)
+      }
+    },75)
+     return ()=>clearInterval(interval)
+  },[])
+
   return (
     <div name="home" 
     className=' h-screen w-full bg-gradient-to-b from-black to to-blue-950
@@ -13,9 +31,9 @@ function Home() {
         <div className='max-w-screen-lg mx-auto flex flex-col 
          items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-              <h2 className='text-4xl sm:text-7xl text-white
-              font-bold'>Im a React , Tailwindcss  and C++ developer</h2>
-              <p className='text-gray-500 py-4 max-w-md'>
+              <h2 className={`text-4xl sm:text-7xl text-white
+              font-bold ${isvisible ? 'opacity-100':'opacity-0'} transition-opacity duration-1000` }>{text}</h2>
+              <p className={`text-gray-500 py-4 max-w-md `}>
                 I have some experience in Html ,Javascript and CSS
               </p>
               <div>
@@ -28,7 +46,9 @@ function Home() {
               </div>
            </div>
            <div>
-            < img src={Hero} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+           < img src={Hero} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full '/>
+           
+
            </div>
         </div>
     </div>
